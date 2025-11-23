eebd97380e5c7d417614e7ad88cc527083998c89
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2025-11-23 11:59:37 +0600

    initial project setup

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+
+import prisma from '@/lib/prisma';
+import * as bcrypt from 'bcryptjs';
+
+async function main() {
+  const email = 'admin-system@gmail.com';
+  const password = 'Admin@123';
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: {
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
